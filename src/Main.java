7ab7c2876c2ebb4fdf07904dfe3861a048b20996
João Paulo de Oliveira <jpos.usp@gmail.com>
2023-12-10 19:45:16 -0300

    change to avoid running tasks on empty list

--- src/Main.java
+++ src/Main.java
@@ -10,16 +10,20 @@ public class Main {
         int LIST_SIZE = 100;
         BDFileReader reader = new BDFileReader();
         List<String> words = reader.readFile("/src/files/bd.txt");
+        if (words == null) {
+            System.out.println("Erro ao ler o arquivo bd.txt. Verifique o caminho do arquivo");
+        } else {
+            List<Executor> executors = new ArrayList<>();
+            executors.add(Executor.MULTI_THREAD);
+            executors.add(Executor.SINGLE_THREAD);
 
-        List<Executor> executors = new ArrayList<>();
-        executors.add(Executor.MULTI_THREAD);
-        executors.add(Executor.SINGLE_THREAD);
+            for (Executor executor : executors) {
+                System.out.println(executor.name());
 
-        for (Executor executor : executors) {
-            System.out.println(executor.name());
-
-            ExecutorRunner executorRunner = new ExecutorRunner(executor);
-            executorRunner.runTask(words, LIST_SIZE);
+                ExecutorRunner executorRunner = new ExecutorRunner(executor);
+                executorRunner.runTask(words, LIST_SIZE);
+            }
         }
+
     }
 }
